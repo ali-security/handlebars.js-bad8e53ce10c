14465fbccab2f94a53e82a8a133358785052b5f4
Aaron Belenky <aaron.b@sealsecurity.io>
2026-06-28 19:59:40 +0300

    CVE-2019-19919

--- spec/security.js
+++ spec/security.js
@@ -77,4 +77,20 @@ describe('security issues', function() {
       }
     });
   });
+
+  describe('GH-1595', function() {
+    it('properties, that are required to be enumerable', function() {
+      shouldCompileTo('{{constructor}}', {}, '');
+      shouldCompileTo('{{__defineGetter__}}', {}, '');
+      shouldCompileTo('{{__defineSetter__}}', {}, '');
+      shouldCompileTo('{{__lookupGetter__}}', {}, '');
+      shouldCompileTo('{{__proto__}}', {}, '');
+
+      shouldCompileTo('{{lookup this "constructor"}}', {}, '');
+      shouldCompileTo('{{lookup this "__defineGetter__"}}', {}, '');
+      shouldCompileTo('{{lookup this "__defineSetter__"}}', {}, '');
+      shouldCompileTo('{{lookup this "__lookupGetter__"}}', {}, '');
+      shouldCompileTo('{{lookup this "__proto__"}}', {}, '');
+    });
+  });
 });
